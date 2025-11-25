9c2625f6681fdeef6cc7e7c856ebd48491428957
joseantoio1234 <jagonzalezm38@educarex.es>
2025-11-25 17:50:13 +0100

    he añadido las letras del modo de dificultad facil

--- server/constants.js
+++ server/constants.js
@@ -1,4 +1,9 @@
-export const fragments = [
+export const fragments = {
+  facil:[
+    "a", "b", "c", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y",
+    "z", "ll",
+  ],
+  normal:[  
   "pla", "ple", "pli", "plo", "flu", "tra", "tre", "tri", "tro", "tru",
   "car", "cer", "cor", "cur", "cla", "cle", "cli", "clo", "clu",
   "pan", "pen", "pin", "pon", "pun", "tar", "ter", "tir", "tor", "tur",
@@ -9,4 +14,8 @@ export const fragments = [
   "mal", "mel", "mil", "mol", "mul", "tan", "ten", "tin", "ton", "tun",
   "fel", "fil", "fal", "fol", "ful", "ven", "ver", "vir", "vor", "vul",
   "zar", "zer", "zir", "zor", "zur", "bal", "bel", "bil", "bol", "bul"
-];
+],
+dificil:[
+
+]
+};
