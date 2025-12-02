ea583488bdbeae8238d928d8dbe29e2573c63df8
joseantoio1234 <jagonzalezm38@educarex.es>
2025-12-02 16:40:37 +0100

    cambios en letras

--- server/constants.js
+++ server/constants.js
@@ -1,5 +1,5 @@
 export const fragments = {
-  facil:[
+  easy:[
     "a", "b", "c", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y",
     "z", "ll",
   ],
@@ -15,7 +15,16 @@ export const fragments = {
   "fel", "fil", "fal", "fol", "ful", "ven", "ver", "vir", "vor", "vul",
   "zar", "zer", "zir", "zor", "zur", "bal", "bel", "bil", "bol", "bul"
 ],
-dificil:[
-
+dificult:[
+    "pla", "ple", "pli", "plo", "flu", "tra", "tre", "tri", "tro", "tru",
+  "car", "cer", "cor", "cur", "cla", "cle", "cli", "clo", "clu",
+  "pan", "pen", "pin", "pon", "pun", "tar", "ter", "tir", "tor", "tur",
+  "bra", "bre", "bri", "bro", "bru", "dra", "dre", "dri", "dro", "dru",
+  "gal", "gel", "gil", "gol", "gul", "sal", "sel", "sil", "sol", "sul",
+  "per", "por", "pir", "par", "pur", "mar", "mer", "mir", "mor", "mur",
+  "cas", "ces", "cis", "cos", "cus", "lan", "len", "lin", "lon", "lun",
+  "mal", "mel", "mil", "mol", "mul", "tan", "ten", "tin", "ton", "tun",
+  "fel", "fil", "fal", "fol", "ful", "ven", "ver", "vir", "vor", "vul",
+  "zar", "zer", "zir", "zor", "zur", "bal", "bel", "bil", "bol", "bul"
 ]
 };
